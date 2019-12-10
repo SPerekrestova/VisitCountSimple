58ad1e5497d6797808395c38bb1c60aa228ce1d9
SPerekrestova <Lumos0202>
2019-12-10 15:31:57 +0300

    Made iterator for the whole file

--- src/main/java/app/parsing/Read.java
+++ src/main/java/app/parsing/Read.java
@@ -4,26 +4,35 @@ import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.Iterator;
 
 public class Read {
 
     public static void readFromExcel(String file) throws IOException {
         HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream(file));
-        HSSFSheet myExcelSheet = myExcelBook.getSheet("Group");
-        HSSFRow row = myExcelSheet.getRow(0);
+        HSSFSheet myExcelSheet = myExcelBook.getSheetAt(0);
 
-        if(row.getCell(0).getCellType() == HSSFCell.CELL_TYPE_NUMERIC){
-            String card = String.valueOf((int)row.getCell(0).getNumericCellValue());
-            System.out.println("card : " + card);
-        }
-
-        if(row.getCell(1).getCellType() == HSSFCell.CELL_TYPE_STRING){
-            String name = row.getCell(1).getStringCellValue();
-            System.out.println("name :" + name);
+        for (Row myrow : myExcelSheet) {
+            for (Cell mycell : myrow) {
+                switch (mycell.getCellType()) {
+                    case HSSFCell.CELL_TYPE_NUMERIC:
+                        String card = String.valueOf((int)mycell.getNumericCellValue());
+                        System.out.println("card : " + card);
+                        break;
+                    case HSSFCell.CELL_TYPE_STRING:
+                        String name = mycell.getStringCellValue();
+                        System.out.println("name :" + name);
+                        break;
+                    default:
+                }
+            }
         }
         myExcelBook.close();
     }
