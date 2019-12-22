4752e1a762b3d6e173f35abf3ee4edb32ef7816a
SPerekrestova <Lumos0202>
2019-12-22 16:18:34 +0300

    Done with select query

--- src/main/java/app/parsing/Write.java
+++ src/main/java/app/parsing/Write.java
@@ -7,42 +7,45 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
 
 public class Write {
     @SuppressWarnings("deprecation")
-    public static void writeIntoExcel(String file) throws FileNotFoundException, IOException, FileNotFoundException {
-        Workbook book = new HSSFWorkbook();
-        Sheet sheet = book.createSheet("Birthdays");
-
-        // Нумерация начинается с нуля
-        Row row = sheet.createRow(0);
-
-        // Мы запишем имя и дату в два столбца
-        // имя будет String, а дата рождения --- Date,
-        // формата dd.mm.yyyy
-        Cell name = row.createCell(0);
-        name.setCellValue("John");
-
-        Cell birthdate = row.createCell(1);
-
-        DataFormat format = book.createDataFormat();
-        CellStyle dateStyle = book.createCellStyle();
-        dateStyle.setDataFormat(format.getFormat("dd.mm.yyyy"));
-        birthdate.setCellStyle(dateStyle);
-
-
-        // Нумерация лет начинается с 1900-го
-        birthdate.setCellValue(new Date(110, 10, 10));
-
-        // Меняем размер столбца
-        sheet.autoSizeColumn(1);
-
-        // Записываем всё в файл
-        try {
-            book.write(new FileOutputStream(file));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        book.close();
+    public static void writeIntoExcel(HashMap groupMap, Map<Integer, SortedSet<String>> scheduleMap) throws FileNotFoundException, IOException, FileNotFoundException {
+//        Workbook book = new HSSFWorkbook();
+//        Sheet sheet = book.createSheet("Birthdays");
+//
+//        // Нумерация начинается с нуля
+//        Row row = sheet.createRow(0);
+//
+//        // Мы запишем имя и дату в два столбца
+//        // имя будет String, а дата рождения --- Date,
+//        // формата dd.mm.yyyy
+//        Cell name = row.createCell(0);
+//        name.setCellValue("John");
+//
+//        Cell birthdate = row.createCell(1);
+//
+//        DataFormat format = book.createDataFormat();
+//        CellStyle dateStyle = book.createCellStyle();
+//        dateStyle.setDataFormat(format.getFormat("dd.mm.yyyy"));
+//        birthdate.setCellStyle(dateStyle);
+//
+//
+//        // Нумерация лет начинается с 1900-го
+//        birthdate.setCellValue(new Date(110, 10, 10));
+//
+//        // Меняем размер столбца
+//        sheet.autoSizeColumn(1);
+//
+//        // Записываем всё в файл
+//        try {
+//            book.write(new FileOutputStream(file));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        book.close();
     }
 }
--- src/main/java/app/servlets/CreateFileServlet.java
+++ src/main/java/app/servlets/CreateFileServlet.java
@@ -1,41 +1,66 @@
 package app.servlets;
 
 import app.database.ExecuteStatement;
-import app.model.Model;
+import app.parsing.Write;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 @WebServlet("/create")
 public class CreateFileServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    private ExecuteStatement executeStatement;
-
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String selectGroup = "SELECT card, student FROM main.'Group' WHERE group_name = 'ИСТ-722';";
         String selectSchedule = "SELECT lesson_num, day, time FROM main.'Schedule' WHERE group_name = 'ИСТ-722';";
-
-
+        HashMap<Integer, String> groupMap = new HashMap<>();
+        HashMap<Integer, SortedSet<String>> lessons = new HashMap<>();
+        SortedSet<String> time = new TreeSet<String>();
+        int day = 0;
         try {
-            executeStatement.initConnection();
-            ResultSet rs =  executeStatement.ExecuteSelect(selectGroup);
+            ExecuteStatement.initConnection();
+            ResultSet rs =  ExecuteStatement.ExecuteSelect(selectGroup);
             while (rs.next()) {
-                String lastName = rs.getString("card");
-                System.out.println(lastName + "\n");
+                String card = rs.getString("card");
+                String student = rs.getString("student");
+                groupMap.put(Integer.valueOf(card), student);
             }
-            executeStatement.closeConnection();
-        } catch (SQLException | ClassNotFoundException e) {}
+            LinkedHashMap<Integer, String> sortedGroupMap =
+                    groupMap.entrySet().stream().
+                            sorted(Map.Entry.comparingByValue()).
+                            collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                                    (e1, e2) -> e1, LinkedHashMap::new));
+            ExecuteStatement.closeConnection();
+
+            ExecuteStatement.initConnection();
+            ResultSet rs2 =  ExecuteStatement.ExecuteSelect(selectSchedule);
+            while (rs2.next()) {
+                day = Integer.parseInt(rs2.getString("day"));
+                //день помещается впервые
+                if (!lessons.containsKey(day)) {
+                    time = new TreeSet<>();
+                    time.add(rs2.getString("time"));
+                } //день уже есть
+                else {
+                    time.add(rs2.getString("time"));
+                }
+                lessons.put(day, time);
+            }
+            Map<Integer, SortedSet<String>> sortedLessons = new TreeMap<Integer, SortedSet<String>>(lessons);
+            ExecuteStatement.closeConnection();
+
+            Write.writeIntoExcel(sortedGroupMap, sortedLessons);
+        } catch (Exception e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
     }
 
 
